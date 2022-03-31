129a8abbed14e805bdc83b725fc6be8164c95806
1824975 <1824975@PE326-08>
2022-03-31 14:53:01 -0400

    Started creating Presenter

---
+++ Presenter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Budget;
+
+namespace HomeBudgetWPF
+{
+    class Presenter
+    {
+        //const string DEFAULT_FILENAME = "%homepath%\\BudgetFiles\\budget.db";
+        const string DEFAULT_FILENAME = "./budget.db";
+        HomeBudget model;
+        public Presenter()
+        {
+            model = new HomeBudget(DEFAULT_FILENAME);
+        }
+
+    }
+}
