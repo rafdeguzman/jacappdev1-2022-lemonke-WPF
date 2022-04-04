fd9834d5001d6a6b6811977a30384b42f9240440
1824975 <1824975@PE326-08>
2022-04-04 14:56:25 -0400

    Creating Add Categories from Combo Box

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,25 +18,79 @@ namespace HomeBudgetWPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+   
     public partial class MainWindow : Window, IView
     {
+        private string category;
         private readonly Presenter presenter;
+        //for use with combo box items
+        private List<string> items = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
             presenter = new Presenter(this);
         }
 
-        private void btnAdd1_Click(object sender, RoutedEventArgs e)
-        {
-            cmbCategories.Items.Add(txtBox1.Text);
-            presenter.AddCategory();
-        }
-
         public string GetStringInput()
         {
-            return txtBox1.Text;
+            return category;
         }
 
+        //Gorav's code
+        public void DisplayCategories(List<Category> categories)
+        {
+            cmbCategory.DisplayMemberPath = "Description";
+            foreach (Category Displaycategories in categories)
+            {
+                cmbCategory.Items.Add(Displaycategories);
+            }
+        }
+
+        //Stackoverflow references: https://stackoverflow.com/questions/4609847/wpf-combobox-with-iseditable-true-how-can-i-indicate-that-no-match-was-found
+        private void cmbCategories_KeyDown(object sender, KeyEventArgs e)
+        {
+            //textbox part of editable combo box
+            TextBox tb = cmbCategories.Template.FindName("PART_EditableTextBox", cmbCategories) as TextBox;
+
+            //dont run if tb.Text is empty
+            if (!(tb.Text == String.Empty || tb.Text == " "))
+            {
+                foreach (var item in cmbCategories.Items)
+                    items.Add(item.ToString());
+                
+
+                //if items doesnt have tb.text and return is pressed
+                if (e.Key == Key.Return && !items.Contains(tb.Text))
+                {
+                    //verify that user wants to add
+                    string messageBoxText = "Are you sure you want to add " + tb.Text + " as a category?";
+                    string caption = "Confirm";
+                    MessageBoxButton button = MessageBoxButton.YesNoCancel;
+                    MessageBoxImage icon = MessageBoxImage.Question;
+                    MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
+                    if(result == MessageBoxResult.Yes)
+                    {
+                        cmbCategories.Items.Add(tb.Text);
+                        category = tb.Text;
+                        presenter.AddCategory();
+                    }
+                    else
+                    {
+                        //do nothing
+                    }
+                }
+                else if (e.Key == Key.Return && items.Contains(tb.Text))
+                {
+                    //taken from https://docs.microsoft.com/en-us/dotnet/desktop/wpf/windows/how-to-open-message-box?view=netdesktop-6.0
+                    string messageBoxText = tb.Text + " is already on the list.";
+                    string caption = "Error";
+                    MessageBoxButton button = MessageBoxButton.OK;
+                    MessageBoxImage icon = MessageBoxImage.Warning;
+                    MessageBoxResult result;
+
+                    result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+                }
+            }
+        }
     }
 }
--- Presenter.cs
+++ Presenter.cs
@@ -11,37 +11,40 @@ namespace HomeBudgetWPF
     class Presenter
     {
         const string DEFAULT_PATH = "\\Documents\\BudgetFiles\\";
-        const string DEFAULT_FILENAME = "budget.db";
-        const string DEFAULT_FILEPATH = DEFAULT_PATH + DEFAULT_FILENAME;
-        //const string DEFAULT_FILENAME = "./budget.db";
+        //const string DEFAULT_FILENAME = "budget.db";
+        //const string DEFAULT_FILEPATH = DEFAULT_PATH + DEFAULT_FILENAME;
+        const string DEFAULT_FILENAME = "./budget.db";
         HomeBudget model;
         IView view;
-        
+
         public Presenter(IView v)
         {
-            model = new HomeBudget("newDB");
+            if (!Directory.Exists(DEFAULT_PATH))
+                Directory.CreateDirectory(DEFAULT_PATH);
+
+            model = new HomeBudget(DEFAULT_FILENAME, !File.Exists(DEFAULT_FILENAME));
             view = v;
 
-            view.DisplayCategories(PopulateCategories());
+        }
+        public void AddCategory()
+        {
+            model.categories.Add(view.GetStringInput());
         }
 
-        public List<Category> PopulateCategories()
+        public List<string> PopulateCategories()
         {
-
             List<Category> categoriesList = new();
             foreach (Category categories in model.categories.List())
             {
                 categoriesList.Add(categories);
             }
 
-            return categoriesList;
+            List<string> categoriesListString = new List<string>();
+            foreach (var category in categoriesList)
+                categoriesListString.Add(category.ToString());
+
+            return categoriesListString;
         }
 
-        public Expense AddExpense()
-        {
-
-
-            return 
-        }
     }
 }
