395fcd1949e90164feb1b91a8c52e028ebdacd84
1824975 <1824975@PE326-08>
2022-04-05 12:43:22 -0400

    made thigns smaller

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -56,6 +56,41 @@ namespace HomeBudgetWPF
             }
         }
 
+        private void AddCategoriesToList()
+        {
+            foreach (var item in cmbCategories.Items)
+                categories.Add(item.ToString());
+        }
+        private void AddCategoryToDB(string categoryName)
+        {
+            //verify that user wants to add
+            string messageBoxText = "Are you sure you want to add " + categoryName + " as a category?";
+            string caption = "Confirm";
+            MessageBoxButton button = MessageBoxButton.YesNoCancel;
+            MessageBoxImage icon = MessageBoxImage.Question;
+            MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
+            if (result == MessageBoxResult.Yes)
+            {
+                cmbCategories.Items.Add(categoryName);
+                category = categoryName;
+                presenter.AddCategory(cmbCategoryTypes.SelectedIndex);
+            }
+            else
+            {
+                //do nothing
+            }
+        }
+        private void ShowCategoryError(string categoryName)
+        {
+            //taken from https://docs.microsoft.com/en-us/dotnet/desktop/wpf/windows/how-to-open-message-box?view=netdesktop-6.0
+            string messageBoxText = categoryName + " is already on the list.";
+            string caption = "Error";
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult result;
+
+            result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+        }
 
         //Stackoverflow references: https://stackoverflow.com/questions/4609847/wpf-combobox-with-iseditable-true-how-can-i-indicate-that-no-match-was-found
         private void cmbCategories_KeyDown(object sender, KeyEventArgs e)
@@ -66,40 +101,14 @@ namespace HomeBudgetWPF
             //dont run if tb.Text is empty
             if (!(tb.Text == String.Empty || tb.Text == " "))
             {
-                foreach (var item in cmbCategories.Items)
-                    categories.Add(item.ToString());
-                
+                AddCategoriesToList();
+
                 //if items doesnt have tb.text and return is pressed
                 if (e.Key == Key.Return && !categories.Contains(tb.Text))
-                {
-                    //verify that user wants to add
-                    string messageBoxText = "Are you sure you want to add " + tb.Text + " as a category?";
-                    string caption = "Confirm";
-                    MessageBoxButton button = MessageBoxButton.YesNoCancel;
-                    MessageBoxImage icon = MessageBoxImage.Question;
-                    MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
-                    if(result == MessageBoxResult.Yes)
-                    {
-                        cmbCategories.Items.Add(tb.Text);
-                        category = tb.Text;   
-                        presenter.AddCategory(cmbCategoryTypes.SelectedIndex);
-                    }
-                    else
-                    {
-                        //do nothing
-                    }
-                }
+                    AddCategoryToDB(tb.Text);   
+                
                 else if (e.Key == Key.Return && categories.Contains(tb.Text))
-                {
-                    //taken from https://docs.microsoft.com/en-us/dotnet/desktop/wpf/windows/how-to-open-message-box?view=netdesktop-6.0
-                    string messageBoxText = tb.Text + " is already on the list.";
-                    string caption = "Error";
-                    MessageBoxButton button = MessageBoxButton.OK;
-                    MessageBoxImage icon = MessageBoxImage.Warning;
-                    MessageBoxResult result;
-
-                    result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
-                }
+                    ShowCategoryError(tb.Text);
             }
         }
 
@@ -111,40 +120,14 @@ namespace HomeBudgetWPF
             //dont run if tb.Text is empty
             if (!(tb.Text == String.Empty || tb.Text == " "))
             {
-                foreach (var item in cmbCategories.Items)
-                    categories.Add(item.ToString());
+                AddCategoriesToList();
 
                 //if items doesnt have tb.text and return is pressed
                 if (!categories.Contains(tb.Text))
-                {
-                    //verify that user wants to add
-                    string messageBoxText = "Are you sure you want to add " + tb.Text + " as a category?";
-                    string caption = "Confirm";
-                    MessageBoxButton button = MessageBoxButton.YesNoCancel;
-                    MessageBoxImage icon = MessageBoxImage.Question;
-                    MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
-                    if (result == MessageBoxResult.Yes)
-                    {
-                        cmbCategories.Items.Add(tb.Text);
-                        category = tb.Text;
-                        presenter.AddCategory(cmbCategoryTypes.SelectedIndex);
-                    }
-                    else
-                    {
-                        //do nothing
-                    }
-                }
-                else if (categories.Contains(tb.Text))
-                {
-                    //taken from https://docs.microsoft.com/en-us/dotnet/desktop/wpf/windows/how-to-open-message-box?view=netdesktop-6.0
-                    string messageBoxText = tb.Text + " is already on the list.";
-                    string caption = "Error";
-                    MessageBoxButton button = MessageBoxButton.OK;
-                    MessageBoxImage icon = MessageBoxImage.Warning;
-                    MessageBoxResult result;
+                    AddCategoryToDB(tb.Text);
 
-                    result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
-                }
+                else if (categories.Contains(tb.Text))
+                    ShowCategoryError(tb.Text);
             }
         }
     }
