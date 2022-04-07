b36df5c85d918942218bf36031deeea1644df2f7
LiamKopke <78220160+LiamKopke@users.noreply.github.com>
2022-04-07 01:20:16 -0400

    added config interface

---
+++ MVP/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace HomeBudgetWPF
+{
+    class Config : ConfigInterface
+    {
+        public string getDirectory => ConfigurationManager.AppSettings.Get("defaultFileDirectory");
+        public string getFileName => ConfigurationManager.AppSettings.Get("defaultFileName");
+
+        public string lastUsedFilePath => ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None).AppSettings.Settings["lastUsedFilePath"].Value;
+
+        public string currentFile => ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None).AppSettings.Settings["currentFile"].Value;
+
+        public string firstTimeUser => ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None).AppSettings.Settings["firstTimeUser"].Value;
+        public void saveConfig()
+        {
+            ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None).Save(ConfigurationSaveMode.Modified);
+        }
+    }
+}
---
+++ MVP/ConfigInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBudgetWPF
+{
+    public interface ConfigInterface
+    {
+        string getDirectory { get; }
+        string getFileName { get; }
+        string lastUsedFilePath { get; }
+        string currentFile { get; }
+
+        string firstTimeUser { get; }
+
+        void saveConfig();
+    }
+}
