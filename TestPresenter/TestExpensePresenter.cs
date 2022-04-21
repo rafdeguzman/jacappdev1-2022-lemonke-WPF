aace42017155b0036dd0b38b19f0b92e915b91fa
1824975 <1824975@PE326-08>
2022-04-21 15:49:08 -0400

    im losing my mind

---
+++ TestPresenter/TestExpensePresenter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeBudgetWPF;
+using Budget;
+namespace TestPresenter
+{
+    public class TestExpenseView : ExpenseInterface
+    {
+        public bool CheckUserInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayCategories(List<Category> categories)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplaySameAsLastInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetUserInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LastInput(string categories, string date, string amount, string description, string creditFlag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetText()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class TestExpensePresenter
+    {
+
+    }
+}
