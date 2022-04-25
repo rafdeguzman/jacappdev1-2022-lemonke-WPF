690a3a5a6c3eaa5cdb21b5cd2e15dbe280de1fea
rafdeguzman <78217484+rafdeguzman@users.noreply.github.com>
2022-04-25 00:19:53 -0400

    Update TestMainPresenter.cs

--- TestPresenter/TestMainPresenter.cs
+++ TestPresenter/TestMainPresenter.cs
@@ -33,17 +33,101 @@ namespace TestPresenter
             config = new Config();
         }
 
-    }
-    public class TestPresenter
-    {
-        [Fact]
-        public void TestConstructor()
+        public class TestPresenter
         {
-            TestView view = new TestView();
-            Presenter p = new Presenter(view, true);
-            Assert.IsType<Presenter>(p);
-            Assert.True(view.calledShowFilesCreated);
-            Assert.True(view.calledShowFirstTimeMessage);
+            [Fact]
+            public void TestConstructor()
+            {
+                TestView view = new TestView();
+                Presenter p = new Presenter(view, true);
+                Assert.IsType<Presenter>(p);
+                Assert.True(view.calledShowFilesCreated);
+                Assert.True(view.calledShowFirstTimeMessage);
+
+                TestCategoryView view1 = new TestCategoryView();
+                CategoryPresenter p1 = new CategoryPresenter(view1);
+                Assert.IsType<CategoryPresenter>(p1);
+                Assert.True(view1.calledDisplayCategories);
+                Assert.True(view1.calledDisplayCategoryTypes);
+
+                TestExpenseView view2 = new TestExpenseView();
+                ExpensePresenter p2 = new ExpensePresenter(view2);
+                Assert.IsType<ExpensePresenter>(p2);
+                Assert.True(view2.calledDisplayCategories);
+
+                TestExpenseView view3 = new TestExpenseView();
+                ExpensePresenter p3 = new ExpensePresenter(view3);
+                p3.AddExpense(DateTime.Now, 1, 100, "abc", true);
+                Assert.True(view3.calledLastInput);
+            }
+        }
+    }
+    
+    public class TestCategoryView : CategoryInterface
+    {
+        public bool calledDisplayCategories;
+        public bool calledDisplayCategoryTypes;
+        public bool calledGetStringInput;
+        Config config;
+        public void DisplayCategories(List<string> categories)
+        {
+            calledDisplayCategories = true;
+        }
+
+        public void DisplayCategoryTypes(List<string> categoryTypes)
+        {
+            calledDisplayCategoryTypes = true;
+        }
+
+        public string GetStringInput()
+        {
+            calledGetStringInput = true;
+            return String.Empty;
+        }
+    }
+    
+    public class TestExpenseView : ExpenseInterface
+    {
+        public bool calledCheckUserInput;
+        public bool calledDisplayCategories;
+        public bool calledDisplaySameAsLastInput;
+        public bool calledGetUserInput;
+        public bool calledLastInput;
+        public bool calledResetText;
+        Config config;
+        public bool CheckUserInput()
+        {
+            calledCheckUserInput = true;
+            return calledCheckUserInput;
+        }
+
+        public void DisplayCategories(List<Category> categories)
+        {
+            calledDisplayCategories = true;
+        }
+
+        public void DisplaySameAsLastInput()
+        {
+            calledDisplaySameAsLastInput = true;
+        }
+
+        public void GetUserInput()
+        {
+            calledGetUserInput = true;
+        }
+
+        public void LastInput(string categories, string date, string amount, string description, string creditFlag)
+        {
+            calledLastInput = true;
+        }
+
+        public void ResetText()
+        {
+            calledResetText = true;
+        }
+        public TestExpenseView()
+        {
+            config = new Config();
         }
     }
     
