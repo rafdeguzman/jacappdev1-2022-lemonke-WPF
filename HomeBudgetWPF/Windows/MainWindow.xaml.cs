7bf0dc55609286d2d08e029050007ab219433cee
1824975 <1824975@PE326-08>
2022-04-26 14:13:16 -0400

    lol

--- HomeBudgetWPF/Windows/MainWindow.xaml.cs
+++ HomeBudgetWPF/Windows/MainWindow.xaml.cs
@@ -147,6 +147,7 @@ namespace HomeBudgetWPF
             {
                 presenter.DeleteExpense(ExpenseId);
                 Refresh();
+                //if(ExpenseId >= dataBudgetLists.Items.Count)
             }
         }
 
